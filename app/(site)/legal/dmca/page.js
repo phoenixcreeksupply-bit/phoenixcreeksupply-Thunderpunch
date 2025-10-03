5138501d2aea3fcd82c5ecb73251718c389e18b3
phoenixcreeksupply-bit <phoenixcreeksupply@gmail.com>
2025-10-03 01:26:16 -0600

    Create page.js

---
+++ app/(site)/legal/dmca/page.js
@@ -0,0 +1,34 @@
+export default function DMCAPage() {
+  return (
+    <main className="prose lg:prose-xl mx-auto px-4 py-10">
+      <h1>DMCA Takedown Policy</h1>
+      <p>Effective Date: October 2025</p>
+
+      <p>
+        Phoenix Creek Supply respects the intellectual property rights of others and 
+        expects our users and affiliates to do the same. It is our policy to respond 
+        to clear notices of alleged copyright infringement that comply with the 
+        Digital Millennium Copyright Act (DMCA).
+      </p>
+
+      <h2>How to File a DMCA Notice</h2>
+      <p>
+        If you believe material hosted on our site infringes your copyright, please send 
+        a written notice including the following:
+      </p>
+      <ul>
+        <li>Your physical or electronic signature.</li>
+        <li>Identification of the copyrighted work you claim is infringed.</li>
+        <li>Identification of the infringing material and its location.</li>
+        <li>Your contact information (email and physical address).</li>
+        <li>A statement that you have a good faith belief the use is not authorized.</li>
+        <li>A statement, under penalty of perjury, that the information in your notice is accurate.</li>
+      </ul>
+
+      <p>
+        Notices can be submitted to:  
+        <a href="mailto:phoenixcreeksupply@gmail.com">phoenixcreeksupply@gmail.com</a>
+      </p>
+    </main>
+  );
+}
