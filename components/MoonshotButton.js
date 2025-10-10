924fddd486ab79670977d377d35eac2a5d5d6135
phoenixcreeksupply-bit <phoenixcreeksupply@gmail.com>
2025-10-10 13:38:47 -0600

    button adjust

--- components/MoonshotButton.js
+++ components/MoonshotButton.js
@@ -20,14 +20,10 @@ export default function MoonshotButton({ href }) {
   return (
     <button
       onClick={handleClick}
-      className="inline-flex items-center justify-center p-0 w-auto h-auto bg-transparent rounded-none shadow-none"
       aria-label="Buy PCS Token on Moonshot"
+      className="fixed bottom-6 left-6 z-50 inline-flex items-center justify-center w-14 h-14 rounded-full bg-white/90 shadow-lg hover:scale-105 transition-transform"
     >
-      <img
-        src="/images/PCSmoonToken.jpg"
-        alt="Buy PCS Token on Moonshot"
-        className="w-48 h-auto rounded-full bg-transparent"
-      />
+      <img src="/images/PCSmoonToken.jpg" alt="Buy PCS Token on Moonshot" className="w-10 h-10" />
     </button>
   );
 }
