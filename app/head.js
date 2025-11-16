2b00c9362806defee613b64a7cefd6e5c2d0b42d
phoenixcreeksupply-bit <phoenixcreeksupply@gmail.com>
2025-11-15 23:58:13 -0700

    Remove title and meta tags from Head component
    Removed old title and meta description from Head component.

--- app/head.js
+++ app/head.js
@@ -1,12 +1,6 @@
 export default function Head() {
   return (
     <>
-      <title>Phoenix Creek Supply | Lead Yourself, Live With Intent</title>
-      <meta
-        name="description"
-        content="Phoenix Creek Supply — rugged tools, digital kits, and gear built for those who lead themselves and live with intent."
-      />
-      {/<script type="text/javascript" src="http://classic.avantlink.com/affiliate_app_confirm.php?mode=js&authResponse=7d4cc152694097df60f864a9205b11db69b99323"></script>/}
       <script
         type="text/javascript"
         src="http://classic.avantlink.com/affiliate_app_confirm.php?mode=js&authResponse=01d685267d651f03926454eb4973f82277048b2b"
