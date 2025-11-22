c49b8d62ac5309a212ab135b876f57d9e426221f
phoenixcreeksupply-bit <phoenixcreeksupply@gmail.com>
2025-11-22 03:24:31 -0700

    Add GlobalMarketButton component

---
+++ components/GlobalMarketButton.jsx
@@ -0,0 +1,40 @@
+"use client";
+
+export default function GlobalMarketButton({
+  className = "",
+  label = "Global Market Access",
+}) {
+  return (
+    <div className={className}>
+      <a
+        href="https://www.dpbolvw.net/click-101573160-15734304"
+        target="_blank"
+        rel="nofollow sponsored noopener"
+        className="
+          inline-block 
+          px-5 py-3 
+          rounded-lg 
+          font-semibold 
+          text-sm
+          bg-neutral-800 
+          text-yellow-400 
+          border border-neutral-700 
+          hover:bg-neutral-700 
+          transition
+          shadow-md
+        "
+      >
+        {label}
+      </a>
+
+      {/* Invisible tracking pixel (required for CJ) */}
+      <img
+        src="https://www.awltovhc.com/image-101573160-15734304"
+        width="1"
+        height="1"
+        style={{ display: "none" }}
+        alt=""
+      />
+    </div>
+  );
+}
